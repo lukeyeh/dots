5fbcae0a744b7cd429497d9462111abd80f7a186
lukeyeh <lukeyeh@proton.me>
2024-02-19 00:54:41 -0500

    add nixos and nvim configs

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+require "user.plugins"
+require "user.options"
+require "user.keymaps"
+require "user.comment"
+require "user.cmp"
+require "user.colorscheme"
+require "user.bufferline"
+require "user.lualine"
---
+++ nvim/lua/user/conform.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_augroup("AutoFormatting", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.rs",
+	group = "AutoFormatting",
+	callback = function()
+		vim.lsp.buf.format({ async = true })
+	end,
+})
