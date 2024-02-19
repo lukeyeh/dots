0efce7b96e83234bdea7ca8b850546863a957d76
lukeyeh <lukeyeh@proton.me>
2024-02-19 01:06:51 -0500

    add some ssh configs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,6 +6,7 @@
     ./hyprland.nix
     ./fish.nix
     ./git.nix
+    ./ssh.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
---
+++ home-manager/ssh.nix
@@ -0,0 +1,9 @@
+{ ... }:
+
+{
+  programs.ssh = {
+    enable = true;
+    startAgent = true;
+  };
+}
+
